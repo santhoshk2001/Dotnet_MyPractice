0bf1b60e9e4b4e5e3aec939a71e96ac61ed36542
santhoshk2001 <santhoshke@infinite.com>
2024-08-13 17:09:15 +0530

    MVC Practices

---
+++ MVC/HtmlHelpersPrj/HtmlHelpersPrj/CustomHelpers/CustomHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HtmlHelpersPrj.CustomHelpers
+{
+    public class CustomHelper
+    {
+    }
+}
---
+++ MVC/ViewModel/ViewModel/Controllers/ViewModelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewModel.Controllers
+{
+    public class ViewModelController : Controller
+    {
+        // GET: ViewModel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
